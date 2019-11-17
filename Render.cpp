f599300cc26b31c950150bc6c9ca88aa1f31c4cc
Jake <meegan118@gmail.com>
2019-11-17 23:43:54 +0000

    refactoring

--- Render.cpp
+++ Render.cpp
@@ -6,104 +6,105 @@
 #include "Triangle.h"
 
 void Render::abg() {
-    Image image(128);
+    Image image(128); // create canvas
     Triangle triangle(
             Vertex(61, 10, RGB(255, 0, 0), UV(0.160268, 0.290086)),
             Vertex(100, 100, RGB(0, 255, 0), UV(0.083611, 0.159907)),
             Vertex(25, 90, RGB(0, 0, 255), UV(0.230169, 0.222781))
-    );
+    ); // create triangle with attributes
 
-    for (unsigned long y = image.get_height()-1; y != -1; y--) {
+    // loop through every pixel
+    for (unsigned long y = image.get_height() - 1; y != -1; y--) {
         for (unsigned long x = 0; x < image.get_width(); x++) {
-            float alpha;
-            float beta;
-            float gamma;
-            triangle.get_barycentric(Point(x, y), alpha, beta, gamma);
+            float alpha, beta, gamma;
+            triangle.get_barycentric(Point(x, y), alpha, beta, gamma); // get barycentric coords
 
+            // get colours and clamp between 0-255
             Color color_r = clamp(100 + (100 * alpha));
             Color color_g = clamp(100 + (100 * beta));
             Color color_b = clamp(100 + (100 * gamma));
 
+            // set pixel to correct colours
             image[x][y].r = color_r;
             image[x][y].g = color_g;
             image[x][y].b = color_b;
         }
     }
 
-    image.toFile("abg");
+    image.toFile("abg"); // output to file
 }
 
 void Render::half_plane() {
-    Image image(128);
+    Image image(128); // create canvas
     Triangle triangle(
             Vertex(61, 10, RGB(255, 0, 0), UV(0.160268, 0.290086)),
             Vertex(100, 100, RGB(0, 255, 0), UV(0.083611, 0.159907)),
             Vertex(25, 90, RGB(0, 0, 255), UV(0.230169, 0.222781))
-    );
+    ); // create triangle with attributes
 
-    for (unsigned long y = image.get_height()-1; y != -1; y--) {
+    // loop through every pixel
+    for (unsigned long y = image.get_height() - 1; y != -1; y--) {
         for (unsigned long x = 0; x < image.get_width(); x++) {
-            float alpha;
-            float beta;
-            float gamma;
-            triangle.get_barycentric(Point(x, y), alpha, beta, gamma);
+            float alpha, beta, gamma;
+            triangle.get_barycentric(Point(x, y), alpha, beta, gamma); // get barycentric coords
 
-
+            // set (r|g|b) to 0 if (alpha|beta|gamma) is less than 0, otherwise set to 255
             image[x][y].r = (alpha < 0.0) ? 0 : 255;
             image[x][y].g = (beta < 0.0) ? 0 : 255;
             image[x][y].b = (gamma < 0.0) ? 0 : 255;
         }
     }
 
-    image.toFile("halfplane");
+    image.toFile("halfplane"); // output to file
 }
 
 void Render::triangle() {
-    Image image(128);
+    Image image(128); // create canvas
     Triangle triangle(
             Vertex(61, 10, RGB(255, 0, 0), UV(0.160268, 0.290086)),
             Vertex(100, 100, RGB(0, 255, 0), UV(0.083611, 0.159907)),
             Vertex(25, 90, RGB(0, 0, 255), UV(0.230169, 0.222781))
-    );
+    ); // create triangle with attributes
 
-    for (unsigned long y = image.get_height()-1; y != -1; y--) {
+    // loop through every pixel
+    for (unsigned long y = image.get_height() - 1; y != -1; y--) {
         for (unsigned long x = 0; x < image.get_width(); x++) {
-            float alpha;
-            float beta;
-            float gamma;
-            triangle.get_barycentric(Point(x, y), alpha, beta, gamma);
+            float alpha, beta, gamma;
+            triangle.get_barycentric(Point(x, y), alpha, beta, gamma); // get barycentric coords
 
-
+            // skip if not in triangle
             if ((alpha < 0.0) || (beta < 0.0) || (gamma < 0.0))
                 continue;
 
+            // set colour to black
             image[x][y].r = 0;
             image[x][y].g = 0;
             image[x][y].b = 0;
         }
     }
 
-    image.toFile("triangle");
+    image.toFile("triangle"); // output to file
 }
 
 void Render::rgb() {
-    Image image(128);
+    Image image(128); // create canvas
     Triangle triangle(
             Vertex(61, 10, RGB(255, 0, 0), UV(0.160268, 0.290086)),
             Vertex(100, 100, RGB(0, 255, 0), UV(0.083611, 0.159907)),
             Vertex(25, 90, RGB(0, 0, 255), UV(0.230169, 0.222781))
-    );
+    ); // create triangle with attributes
 
-    for (unsigned long y = image.get_height()-1; y != -1; y--) {
+    // loop through every pixel
+    for (unsigned long y = image.get_height() - 1; y != -1; y--) {
         for (unsigned long x = 0; x < image.get_width(); x++) {
-            float alpha;
-            float beta;
-            float gamma;
-            triangle.get_barycentric(Point(x, y), alpha, beta, gamma);
+            float alpha, beta, gamma;
+            triangle.get_barycentric(Point(x, y), alpha, beta, gamma); // get barycentric coords
 
+            // skip if not in triangle
             if ((alpha < 0.0) || (beta < 0.0) || (gamma < 0.0))
                 continue;
 
+            // interpolate colours between triangle
             Color color_r = clamp((alpha * triangle.A.color.r) + (beta * triangle.B.color.r)
                                   + (gamma * triangle.C.color.r));
             Color color_g = clamp((alpha * triangle.A.color.g) + (beta * triangle.B.color.g)
@@ -111,119 +112,66 @@ void Render::rgb() {
             Color color_b = clamp((alpha * triangle.A.color.b) + beta * (triangle.B.color.b)
                                   + (gamma * triangle.C.color.b));
 
+            // set pixel colour to interpolated colour
             image[x][y].r = color_r;
             image[x][y].g = color_g;
             image[x][y].b = color_b;
         }
     }
 
-    image.toFile("rgb");
+    image.toFile("rgb"); // output to file
 }
 
 void Render::texture() {
-    Texture texture("earth.ppm");
+    Texture texture("earth.ppm"); // create canvas
 
     Image image(128);
     Triangle triangle(
             Vertex(61, 10, RGB(255, 0, 0), UV(0.160268, 0.290086)),
             Vertex(100, 100, RGB(0, 255, 0), UV(0.083611, 0.159907)),
             Vertex(25, 90, RGB(0, 0, 255), UV(0.230169, 0.222781))
-    );
+    ); // create triangle with attributes
 
-    for (unsigned long y = image.get_height()-1; y != -1; y--) {
+    // loop through every pixel
+    for (unsigned long y = image.get_height() - 1; y != -1; y--) {
         for (unsigned long x = 0; x < image.get_width(); x++) {
-            float alpha;
-            float beta;
-            float gamma;
-            triangle.get_barycentric(Point(x, y), alpha, beta, gamma);
+            float alpha, beta, gamma;
+            triangle.get_barycentric(Point(x, y), alpha, beta, gamma); // get barycentric coords
 
+            // skip if not in triangle
             if ((alpha < 0.0) || (beta < 0.0) || (gamma < 0.0))
                 continue;
 
-            float s = (alpha * (triangle.A.uv.u * texture->get_width()))
+            // get interpolated x coord for texture
+            float i = (alpha * (triangle.A.uv.u * texture->get_width()))
                       + (beta * (triangle.B.uv.u * texture->get_width()))
                       + (gamma * (triangle.C.uv.u * texture->get_width()));
 
-            float t = texture->get_height() - ((alpha * (triangle.A.uv.v * texture->get_height()))
+            // get interpolated y coord for texture (top->down)
+            float j = texture->get_height() - ((alpha * (triangle.A.uv.v * texture->get_height()))
                                                + (beta * (triangle.B.uv.v * texture->get_height()))
                                                + (gamma * (triangle.C.uv.v * texture->get_height())));
 
-            RGB color =  texture[s][t];
-//            RGB color = BilinearLookup(texture, s, t);
+            // get colour from interpolated x/y coords in texture
+            RGB color = texture[i][j];
 
+            // set values of colour from texture
             image[x][y].r = color.r;
             image[x][y].g = color.g;
             image[x][y].b = color.b;
         }
     }
-
-    for (unsigned long y = image.get_height()-1; y != -1; y--) {
-        for (unsigned long x = 0; x < image.get_width(); x++) {
-            float alpha;
-            float beta;
-            float gamma;
-            triangle.get_barycentric(Point(x, y), alpha, beta, gamma);
-
-            if ((alpha < 0.0) || (beta < 0.0) || (gamma < 0.0))
-                continue;
-
-            float s = (alpha * (triangle.A.uv.u * texture->get_width()))
-                      + (beta * (triangle.B.uv.u * texture->get_width()))
-                      + (gamma * (triangle.C.uv.u * texture->get_width()));
-
-            float t = texture->get_height() - ((alpha * (triangle.A.uv.v * texture->get_height()))
-                                               + (beta * (triangle.B.uv.v * texture->get_height()))
-                                               + (gamma * (triangle.C.uv.v * texture->get_height())));
-
-            RGB color =  texture[s][t];
-
-            texture[s][t].r = 255;
-            texture[s][t].g = 0;
-            texture[s][t].b = 0;
-        }
-    }
-    texture->toFile("earth_with_chunk");
-    image.toFile("texture");
+    image.toFile("texture"); // output to file
 }
 
+/*
+ * Sets min/max values of value to 0-255
+ */
 int Render::clamp(int value) {
-    if(value > 255)
+    if (value > 255)
         return 255;
-    else if(value < 0)
+    else if (value < 0)
         return 0;
     else
         return value;
-}
-
-RGB Render::BilinearLookup(Texture &tex, float s, float t) {
-    int i = s;                    // truncates s to get i
-    int j = t;                    // truncates t to get j
-    float sParm = s - i;          // compute s for interpolation
-    float tParm = t - j;          // compute t for interpolation
-
-    // grab four nearest texel colours
-    RGB colour00 = tex[i][j];
-    RGB colour01 = tex[i][j + 1];
-    RGB colour10 = tex[i + 1][j];
-    RGB colour11 = tex[i + 1][j + 1];
-
-    // compute colours on edges
-    RGB colour0;
-
-    colour0.r = colour00.r + tParm * (colour01.r - colour00.r);
-    colour0.g = colour00.g + tParm * (colour01.g - colour00.g);
-    colour0.b = colour00.b + tParm * (colour01.b - colour00.b);
-
-    RGB colour1;
-    colour1.r = colour10.r + tParm * (colour11.r - colour10.r);
-    colour1.g = colour10.g + tParm * (colour11.g - colour10.g);
-    colour1.b = colour10.b + tParm * (colour11.b - colour10.b);
-
-    // compute colour for interpolated texel
-    RGB colour3;
-    colour3.r = colour1.r + sParm * (colour1.r - colour0.r);
-    colour3.g = colour1.g + sParm * (colour1.g - colour0.g);
-    colour3.b = colour1.b + sParm * (colour1.b - colour0.b);
-
-    return colour3;
 }
